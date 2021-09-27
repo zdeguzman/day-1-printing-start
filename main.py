d3a3b6c2fb83d42e9a59a27dbddca1859375783c
Zach <zdeguzman@users.noreply.github.com>
2021-09-27 21:39:47 +0000

    Debugging

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ print("The function is declared like this:")
 print("print('what to print')")
 print("Hello World!\nHello World!")
 print("Hello" + " " + "Angela")
+
+print("Day 1 - String manipulation")
+print('String Concatenation is done with the "+"" sign.')
+print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backlash and n."))
