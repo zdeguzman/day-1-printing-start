51541eb04818770cff40267190ddff29621d4aee
Zach <zdeguzman@users.noreply.github.com>
2021-09-28 15:52:43 +0000

    Added code to print the length of entered string

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ print("Day 1 - String manipulation")
 print('String Concatenation is done with the "+"" sign.')
 print('e.g. print("Hello " + "world")')
 print(("New lines can be created with a backlash and n."))
+print(len("test"))
+print(len(input("Enter a word")))
+
